c473026c9153443670e1133d67eab7c8aecfd17a
Yağmur K. <yagmurrttk@gmail.com>
2024-05-02 14:11:35 +0300

    Add project files.

---
+++ RapidApiProject/Controllers/ImdbTop100MovieController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiProject.Models;
+using System.Text.Json.Serialization;
+
+namespace RapidApiProject.Controllers
+{
+    public class ImdbTop100MovieController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "90bfe31583msh61ed4e8d86d03efp16f34cjsnd0909e365209" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<MovieViewModel>>(body);
+                return View(values);
+            }
+        }
+    }
+}
